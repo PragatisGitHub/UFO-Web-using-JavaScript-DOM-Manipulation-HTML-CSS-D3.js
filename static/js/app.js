9273e62ba9eac559f1674401170a7bcd908933c7
Pragati Mehta <pragati.soni.mehta.career@gmail.com>
2018-08-28 11:01:19 -0400

    Adding project code for the web page for fetching data dynamically

---
+++ static/js/app.js
@@ -0,0 +1,86 @@
+// from data.js
+var Data = data;
+
+var table = d3.select("table")
+var body = d3.select("tbody")
+
+Data.forEach(datum =>
+{
+    // console.log(datum);
+    var row = body.append("tr");
+    row.append("td").text(datum.datetime);
+    row.append("td").text(datum.city.toUpperCase());
+    row.append("td").text(datum.state.toUpperCase());
+    row.append("td").text(datum.country.toUpperCase());
+    row.append("td").text(datum.shape.toUpperCase());
+    row.append("td").text(datum.durationMinutes);
+    row.append("td").text(datum.comments);
+})
+
+function OnSubmit(){
+    d3.select("tbody").html(" ");
+
+    var Date = d3.select("#datetime").property("value").trim();
+    console.log(Date)
+
+    var City = d3.select("#city").property("value").trim();
+    console.log(City.toLowerCase())
+
+    var State = d3.select("#state").property("value").trim();
+    console.log(State.toLowerCase())
+
+    var Country = d3.select("#country").property("value").trim();
+    console.log(Country.toLowerCase())
+
+    var Shape = d3.select("#shape").property("value").trim();
+    console.log(Shape.toLowerCase())
+
+
+    function Datarange(Case){
+        return(Case.datetime == Date || Case.city == City.toLowerCase() || Case.state == State.toLowerCase()
+              || Case.country == Country.toLowerCase() || Case.shape == Shape.toLowerCase())
+        // return(Case.datetime == Date.trim() || Case.city == City.toLowerCase().trim() || Case.state == State.toLowerCase().trim()
+        // || Case.country == Country.toLowerCase().trim() || Case.shape == Shape.toLowerCase()).trim()      
+        // return(Case.datetime == Date && Case.city == City.toLowerCase() && Case.state == State.toLowerCase()
+        // && Case.country == Country.toLowerCase() && Case.shape == Shape.toLowerCase())
+    }
+
+    tableData = Data.filter(Datarange)
+    console.log(tableData);
+
+    // // var myTable = document.getElementById("table");
+    // var myTable = d3.select("table")
+    // // myTable.deleteRow(2)
+    // var rowCount = myTable.rows.length
+    // for (var x=rowCount-1; x>0; x--) 
+    // {
+    // myTable.deleteRow(x)
+    // }
+
+    var table = d3.select("table")
+    var body = d3.select("tbody")
+    
+    tableData.forEach(datum =>
+    {
+        // console.log(datum);
+        var row = body.append("tr");
+        row.append("td").text(datum.datetime);
+        row.append("td").text(datum.city.toUpperCase());
+        row.append("td").text(datum.state.toUpperCase());
+        row.append("td").text(datum.country.toUpperCase());
+        row.append("td").text(datum.shape.toUpperCase());
+        row.append("td").text(datum.durationMinutes);
+        row.append("td").text(datum.comments);
+    })
+}
+
+function Reset(){
+    d3.select("tbody").html(" ");
+    d3.select("#datetime").property("value"," ")
+    d3.select("#city").property("value"," ")
+    d3.select("#state").property("value"," ")
+    d3.select("#country").property("value"," ")
+    d3.select("#shape").property("value"," ")
+    // d3.select("table").select("tbody").html()  
+    // d3.select("tbody").html() = " "
+}
